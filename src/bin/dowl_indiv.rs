986c501185a5b236364adca2633c298a4676e01a
Colin Finn <cfinn68916@gmail.com>
2025-09-03 11:00:25 -0500

    Download all kakurasu puzzles
    Signed-off-by: Colin Finn <cfinn68916@gmail.com>

---
+++ src/bin/dowl_indiv.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::Write;
+
+async fn fetch_puzzle(url: String) -> Result<String,String> {
+    let response = reqwest::get(url).await;
+    if response.is_err() {
+        return Err(format!(
+            "First unwrap: {}",
+            response.unwrap_err().to_string()
+        ));
+    }
+    let tex2 = response.unwrap().text().await;
+    if tex2.is_err() {
+        return Err(format!("Second unwrap: {}", tex2.unwrap_err().to_string()));
+    }
+    let tex = tex2.unwrap();
+    let finded = tex.find("var task = '");
+    if finded.is_none() {
+        return Err("Task not found".to_string());
+    }
+    let s = finded.unwrap() + "var task = '".len();
+    let tsm = &tex[s..];
+    let also_found = tsm.split("';").nth(0);
+    if also_found.is_none() {
+        return Err("'; not found".to_string());
+    }
+    let task = also_found.unwrap();
+    Ok(task.to_string())
+}
+
+#[tokio::main]
+async fn main() {
+    let mut puzzles=vec![];
+    let mut ident=0;
+    let mut f=File::create_new("kakurasu").unwrap();
+    while ident<100 {
+        let p=fetch_puzzle("https://www.puzzle-kakurasu.com/".to_string()).await;
+        if p.is_ok() {
+            let up=p.unwrap();
+            if !puzzles.contains(&up) {
+                puzzles.push(up.clone());
+                f.write_all(up.as_bytes()).unwrap();
+                f.write_all("\n".as_bytes()).unwrap();
+                ident=0;
+            }else{
+                ident=ident+1;
+            }
+        }else{
+            println!("{}", p.unwrap_err());
+        }
+        println!("{}",puzzles.len());
+    }
+    f.sync_all().unwrap();
+}
